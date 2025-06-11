1de028e76b66365ccd0015f77bba0545d1ebc079
Simos Pavlakis <paulakis.simos@gmail.com>
2025-06-11 05:08:51 +0300

    feat(plugins): make oil floating

--- init.lua
+++ init.lua
@@ -365,17 +365,31 @@ require('lazy').setup({
           is_tree = true,
           use_indicator = true,
         },
+        -- Configuration for the floating window in oil.open_float
         float = {
-          enter = true,
-          border = 'rounded',
+          -- Padding around the floating window
           padding = 2,
-          max_width = 0.8,
-          max_height = 0.8,
+          -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+          max_width = 0,
+          max_height = 0,
+          border = 'rounded',
+          win_options = {
+            winblend = 0,
+          },
+          -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+          get_win_title = nil,
+          -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+          preview_split = 'auto',
+          -- This is the config that will be passed to nvim_open_win.
+          -- Change values here to customize the layout
+          override = function(conf)
+            return conf
+          end,
         },
       }
 
       -- Go up one level
-      vim.keymap.set('n', '-', require('oil').open, {
+      vim.keymap.set('n', '-', require('oil').open_float, {
         desc = 'Oil: Open parent directory',
       })
 
